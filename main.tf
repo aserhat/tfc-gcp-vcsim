3b8be3c1fe968333191184e22297b90550e97751
Allen Serhat <serhata@gmail.com>
2020-04-05 23:38:47 -0400

    Downsoze

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 module "vcsim" {
   source              = "./modules/create-gce-vm"
   server_name_prefix  = "vcsim"
-  server_count        = 1
+  server_count        = 0
   server_machine_type = "g1-small"
   gcp_creds           = "${var.gcp_creds}"
   machine_tags        = ["vcsim"]
